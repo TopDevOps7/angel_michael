958c54dff560fe4f2ec90e36dd6bf82b69cef680
TopDevOps7 <86312703+TopDevOps7@users.noreply.github.com>
2022-05-11 02:39:44 +0900

    updateamazonifile123sadas

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'https://ec2-54-173-30-195.compute-1.amazonaws.com:3000/',
+        target: 'https://54.173.30.195:3000/',
         changeOrigin: true,
         pathRewirte: {
           '^/api': '/api'
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'ec2-54-173-30-195.compute-1.amazonaws.com', // can be overwritten by process.env.HOST
+    host: '54.173.30.195', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
